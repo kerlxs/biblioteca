b42d12f3da354e81b464cf9bce7bae8034de1c82
ruby -v rails -v <trexs26@gmail.com>
2016-05-24 15:26:57 -0500

    Subiendo biblioteca

---
+++ client/recepies/RecipeSingle.js
@@ -0,0 +1,13 @@
+Template.RecipeSingle.onCreated(function(){
+	var self = this;
+	self.autorun(function(){
+		var id = FlowRouter.getParam('id');
+		self.subscribe('singleRecipes', id);
+	});
+});
+Template.RecipeSingle.helpers({
+	recipe: ()=> {
+		var id = FlowRouter.getParam('id');
+		return Recipes.findOne({_id: id});
+	}
+}); 
